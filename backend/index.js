6bed959cd5620efc494f54a04e9e65478886da28
jssdestroyerman <114258377+jssdestroyerman@users.noreply.github.com>
2022-12-31 10:22:33 +0100

    .env update & added .envExemple

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 // Connection à MongoDB via mongoose
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@occursefullstacknodejse.qnnkqu4.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}`
   )
   .then(() => console.log("Connexion à MongoDB réussie"))
   .catch(() => console.log("Connexion à MongoDB échouée"));
@@ -37,5 +37,5 @@ app.use("/api/sauces", sauceRoutes);
 
 // écoute de l'application express sur le port 3000
 app.listen(port, () => {
-  console.log(`Node application started at : http://localhost:${3000}`);
+  console.log(`Node application started at : http://localhost:${port}`);
 });
